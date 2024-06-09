24d5a114acba0cdfc58d249b37db99b5c58a9cc4
chaiwat-S-dev <kenkent122@gmail.com>
2024-06-09 18:21:25 +0700

    refactor: update config

--- utils/config.py
+++ utils/config.py
@@ -7,9 +7,11 @@ INFLUX_URL = os.environ.get("INFLUX_URL", "http://localhost:8086")
 INFLUX_TOKEN = os.environ.get("INFLUX_TOKEN",
                         "Vd3yJYrdCWiTdNqa0iUe24Ou-lGecauOxbiD7hH3SPWPsSxRZx5On0rNS5yw1J1Xi0NkeQDa4F6wx7X84wrjMw==")
 
-DEVICE_TOPIC = [("iot/device", 0)]
+FRASER_INFLUX_URL=os.environ.get("FRASER_INFLUX_URL", "http://localhost:8086")
+FRASER_INFLUX_TOKEN=os.environ.get("FRASER_INFLUX_TOKEN",
+                        "Vd3yJYrdCWiTdNqa0iUe24Ou-lGecauOxbiD7hH3SPWPsSxRZx5On0rNS5yw1J1Xi0NkeQDa4F6wx7X84wrjMw==")
 
-CLIENT_ID = 'middleware_mq_service'
+MQTT_CLIENT_ID = 'mqtt_consumer'
 
 SSL_MODE = bool(os.environ.get("SSL_MODE", True))
 MQTT_HOST = os.environ.get("MQTT_BROKER", "192.168.11.29")
@@ -17,8 +19,6 @@ MQTT_PORT = int(os.environ.get("BROKER_PORT", 1883))
 MQTT_USERNAME = os.environ.get("MQTT_USERNAME", "")
 MQTT_PASSWORD = os.environ.get("MQTT_PASSWORD", "")
 
-AWS_HOST = "a31dvipl38h049-ats.iot.ap-southeast-1.amazonaws.com"
-AWS_PORT = 8883
 CA_PATH = os.path.join(CURRENT_DIR, "cert", os.environ.get("CA_PATH", "AmazonRootCA1.pem"))
 CERT_PATH = os.path.join(CURRENT_DIR, "cert", os.environ.get("CERT_PATH", "iot-core-certificate.pem.crt"))
 KEY_PATH = os.path.join(CURRENT_DIR, "cert", os.environ.get("KEY_PATH", "iot-core-private.pem.key"))
@@ -31,5 +31,11 @@ DATABASES_PORT = os.environ.get("DATABASES_PORT", "5432")
 
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
+
+RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
+QUEUENAME = os.environ.get("QUEUENAME", "consumer_service")
+
+DEVICE_BRAND = os.environ.get("DEVICE_BRAND", "chirpstack")
+HOST_CHIRPSTACK_DEST = int(os.environ.get("HOST_CHIRPSTACK_DEST", "1"))
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
