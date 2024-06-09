08ccef59c3dade154323a982b11f3dd851242489
chaiwat-S-dev <kenkent122@gmail.com>
2024-06-09 18:27:05 +0700

    feat: update device parameter related device model

--- model/models.py
+++ model/models.py
@@ -65,22 +65,39 @@ class DeviceModel(AbstractBaseModel):
     period = db.Column(db.Integer, default=60, nullable=False)
     brand = db.Column(db.String(64), nullable=True)
     output_parameters = db.Column(JSONB, default=dict, nullable=False, server_default='{}')
-    control_parameters = db.Column(JSONB, default=dict, nullable=False, server_default='{}')
-    equipment_id = db.Column(db.String(255), nullable=True)
     app_id = db.Column(db.Integer, default=None, nullable=True)
+    device_parameters = db.relationship("DeviceParameters", back_populates="device")
+    mqtt_host = db.Column(db.Integer)
     
     @property
     def to_dict(self):
+        def get_context_parameter(brand: str) -> list | dict:
+            match brand:
+                case "inhand":
+                    context = {}
+                    for param in self.device_parameters:
+                        if param.slave not in context.keys():
+                            context.update({param.slave: [param.reg_field]})
+                        else:
+                            context[param.slave].append(param.reg_field)
+                    return context
+                case "advantech":
+                    return [{**param.function, "parameter": param.field} for param in self.device_parameters]
+                case _:
+                    return {}
         return {
             "id": self.id,
             "title": self.title,
             "mac_address": self.mac_address,
+            "mac_address_parent": self.mac_address_parent,
             "bucket": self.bucket,
             "measurement": self.measurement,
             "organization": self.organization.code,
             "is_deleted": self.deleted,
             "is_activated": self.is_activated,
-            "brand": self.brand
+            "brand": self.brand,
+            "app_id": self.app_id,
+            "parameters": get_context_parameter(self.brand),
         }
     
     @classmethod
@@ -90,6 +107,41 @@ class DeviceModel(AbstractBaseModel):
  
     def __repr__(self):
         return f"{self.title}:{self.brand}"
+    
+    def save(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
+
+class DeviceParameters(AbstractBaseModel):
+    __tablename__ = 'apis_deviceparameters'
+
+    title = db.Column(db.String(255), nullable=False)
+    field = db.Column(db.String(255), nullable=False)
+    device_id = db.mapped_column(db.ForeignKey("apis_device.id"))
+    device = db.relationship("DeviceModel", back_populates="device_parameters")
+    deadman = db.Column(db.Integer, default=3600, nullable=False)
+    period = db.Column(db.Integer, default=60, nullable=False)
+    slave = db.Column(db.String(100), nullable=True)
+    reg_field = db.Column(db.String(100), nullable=True)
+    function = db.Column(JSONB, default=dict, nullable=False, server_default='{}')
+
+    def __repr__(self):
+        return f"{self.title}:{self.field}"
+
+    @property
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "field": self.field,
+            "slave": self.slave,
+            "reg_field": self.reg_field,
+            "function": self.function,
+        }
     
     def save(self) -> None:
         db.session.add(self)
